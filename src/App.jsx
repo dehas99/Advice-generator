ea0a677822be79b91cc0763b4460fbaaa697bed1
dehas99 <dehas99@gmail.com>
2023-01-26 15:02:32 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import "./App.css";
 import divider from "./assets/pattern-divider-desktop.svg";
 import dice from "./assets/icon-dice.svg";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  useEffect(() => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setAdvice(data);
+      });
+  }, []);
   return (
     <div className="App">
       <div className="Card">
         <p className="advice">ADVICE #117</p>
-        <p className="text">
-          "Its is easy to sit up and take notice, what's difficult is getting up
-          and taking actions"
-        </p>
+        <p className="text">{advice?.slip?.advice}</p>
         <img src={divider} className="divider" alt="divider" />
-        <img src={dice} className="dice" />
+
+        <div className="button">
+          <img src={dice} className="dice" allt="dice" />
+        </div>
       </div>
     </div>
   );
